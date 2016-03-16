2f059a0c47e8c17c01f343f00b7b73e5389f0105
taras_yaroshchuk <taras.yaroshchuk@gmail.com>
2016-03-16 13:21:26 +0200

    Service for sending a mail was created. Properties were added.

---
+++ src/main/java/com/bionic/service/MailService.java
@@ -0,0 +1,21 @@
+package com.bionic.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+    @Autowired
+    private MailSender mailSender;
+
+    public void sendMail(String from,String to, String subject, String msg){
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(msg);
+        mailSender.send(message);
+    }
+}
