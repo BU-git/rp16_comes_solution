2bfa7983e44f1788f3bf5864a28599552cc0e236
taras_yaroshchuk <taras.yaroshchuk@gmail.com>
2016-03-17 22:17:45 +0200

    Removed @EnableWebMvc as it caused some problems.

--- src/main/java/com/bionic/config/MainConfig.java
+++ src/main/java/com/bionic/config/MainConfig.java
@@ -4,15 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
-@EnableWebMvc
+
 @Configuration
 @EnableJpaRepositories("com.bionic.dao")
 @EnableTransactionManagement(proxyTargetClass = true)
