f97dca2793bed49cd9f680b9300562223b2ad14b
taras_yaroshchuk <taras.yaroshchuk@gmail.com>
2016-03-20 22:30:11 +0200

    URI /rest/api/users is now available

--- src/main/java/com/bionic/config/SecurityConfig.java
+++ src/main/java/com/bionic/config/SecurityConfig.java
@@ -5,15 +5,14 @@ package com.bionic.config;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -35,6 +34,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailService).passwordEncoder(getPasswordEncoder());
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web
+				.ignoring()
+				.antMatchers("/rest/api/users");
 	}
 
 	@Override
