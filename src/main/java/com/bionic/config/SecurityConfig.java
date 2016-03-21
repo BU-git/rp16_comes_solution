bf1abc1fe8a5eb8e17d5cb8bfb18e33233c1a426
vitalii.levash <mr.roooot@gmail.com>
2016-03-21 14:42:22 +0200

    Task:Test UserController (add library to pom, and Session change)

--- src/main/java/com/bionic/config/SecurityConfig.java
+++ src/main/java/com/bionic/config/SecurityConfig.java
@@ -10,9 +10,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.AuthenticationEntryPoint;
@@ -37,25 +37,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 	}
 
 	@Override
-	public void configure(WebSecurity web) throws Exception {
-//		web
-//				.ignoring()
-//				.antMatchers("/rest/api/users");
-	}
-
-	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
 		http
 		        .csrf().disable()
 				.exceptionHandling()
 				.authenticationEntryPoint(authenticationEntryPointBean())
+		        .and()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 				.and()
 				.sessionManagement()
 				.and()
 				.authorizeRequests()
 				.antMatchers("/").permitAll()
-				.antMatchers("/rest/api/users/**").authenticated().and().httpBasic();
+				.antMatchers("/rest/api/users/**")
+				.authenticated().and().httpBasic();
 
 	}
 
