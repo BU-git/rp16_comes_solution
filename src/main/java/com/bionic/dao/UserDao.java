6a252b92a49fc71ea13324b6a47d380ca0a3d2c3
Pavel Boiko <boiko.pasha@gmail.com>
2016-03-22 11:28:56 +0200

    Deleted conflicting classes & files.

--- src/main/java/com/bionic/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.bionic.dao;
-
-import com.bionic.model.User;
-
-public interface UserDao {
-
-    public User findByUsername(String username);
-}
--- src/main/java/com/bionic/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.bionic.service;
-
-import com.bionic.model.User;
-
-public interface UserService {
-
-    public User findByUsername(String username);
-}
