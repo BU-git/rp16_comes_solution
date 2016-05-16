011718d4190bb613c4751ebee74b98cea801d95f
taras_yaroshchuk <taras.yaroshchuk@gmail.com>
2016-05-16 17:21:01 +0300

    Add tests for checking if the new shift overlaps the other shifts in DB

--- src/test/java/com/bionic/service/ShiftServiceTest.java
+++ src/test/java/com/bionic/service/ShiftServiceTest.java
@@ -2,7 +2,10 @@ package com.bionic.service;
 
 import com.bionic.config.RootConfig;
 import com.bionic.config.TestPersistenceConfig;
+import com.bionic.exception.shift.impl.ShiftOverlapsException;
 import com.bionic.model.Shift;
+import com.bionic.model.User;
+import org.apache.commons.lang3.time.DateUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,11 +32,14 @@ import static org.junit.Assert.assertTrue;
 @ContextConfiguration(classes = {RootConfig.class, TestPersistenceConfig.class},
         loader = AnnotationConfigWebContextLoader.class)
 @Transactional
-@Rollback
+@Rollback(false)
 public class ShiftServiceTest {
 
     @Autowired
     private ShiftService shiftService;
+
+    @Autowired
+    private UserService userService;
 
     @Test
     public void testGetByUserId() throws Exception {
@@ -41,5 +47,59 @@ public class ShiftServiceTest {
         Shift firstShift = shifts.get(0);
         assertTrue(firstShift.getId().equals(1));
         assertEquals(TEST_USERID, firstShift.getUser().getId());
+    }
+
+    //    Timeline
+    //    startTimeOldShift--startTimeNewShift--endTimeNewShift--endTimeOldShift
+    @Test(expected = ShiftOverlapsException.class)
+    public void testNewShiftInsideOther() throws Exception {
+        List<Shift> shifts = shiftService.getByUserId(TEST_USERID);
+        Shift firstShift = shifts.get(0);
+
+        User user = userService.findById(TEST_USERID);
+
+        Shift newShift = new Shift();
+        newShift.setUser(user);
+        newShift.setStartTime(DateUtils.addMinutes(firstShift.getStartTime(), 5));
+        newShift.setEndTime(DateUtils.addMinutes(firstShift.getEndTime(), -5));
+        newShift.setPause(10L);
+
+        shiftService.addShift(newShift);
+    }
+
+    //    Timeline
+    //    startTimeOldShift--startTimeNewShift--endTimeOldShift--endTimeNewShift
+    @Test(expected = ShiftOverlapsException.class)
+    public void testNewShiftOverlapsByStartTime() throws Exception {
+        List<Shift> shifts = shiftService.getByUserId(TEST_USERID);
+        Shift firstShift = shifts.get(0);
+
+        User user = userService.findById(TEST_USERID);
+
+        Shift newShift = new Shift();
+        newShift.setUser(user);
+        newShift.setStartTime(DateUtils.addMinutes(firstShift.getStartTime(), 5));
+        newShift.setEndTime(DateUtils.addMinutes(firstShift.getEndTime(), 5));
+        newShift.setPause(10L);
+
+        shiftService.addShift(newShift);
+    }
+
+    //    Timeline
+    //    startTimeNewShift--startTimeOldShift--endTimeNewShift--endTimeOldShift
+    @Test(expected = ShiftOverlapsException.class)
+    public void testNewShiftOverlapsByEndTime() throws Exception {
+        List<Shift> shifts = shiftService.getByUserId(TEST_USERID);
+        Shift firstShift = shifts.get(0);
+
+        User user = userService.findById(TEST_USERID);
+
+        Shift newShift = new Shift();
+        newShift.setUser(user);
+        newShift.setStartTime(DateUtils.addMinutes(firstShift.getStartTime(), -20));
+        newShift.setEndTime(DateUtils.addMinutes(firstShift.getEndTime(), 5));
+        newShift.setPause(10L);
+
+        shiftService.addShift(newShift);
     }
 }
